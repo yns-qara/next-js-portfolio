1a9b522c9ddb5e5c6937f335637b1b37062b11fc
yns-qara <younesqara@gmail.com>
2022-11-01 14:34:49 +0100

    👌 styling home

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,20 @@
 import styles from '../styles/Home.module.css'
-
+import Link from 'next/link'
+import Image from 'next/image'
 const Home = () => {
   return (
-    <div className={styles.contaier}>comming soon</div>
+    <div className={styles.contaier}>
+        <div className={styles.left}>
+          <h1>Hello, my name is <span>Younes Qara</span></h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae sunt dolorem, incidunt ullam quibusdam perspiciatis illum totam modi natus dolorum rem! Quo doloribus, at eius, qui deserunt unde, porro voluptatum quae quas laborum aspernatur dolorem labore reiciendis perferendis nihil? Atque quis labore fuga laboriosam. Pariatur.</p>
+          <Link href="#">more about me</Link>
+        </div>
+        <div className={styles.right}>
+          <div>
+            <Image src="/yns.png" width="300" height="397" />
+          </div>
+        </div>
+    </div>
   )
 }
 
