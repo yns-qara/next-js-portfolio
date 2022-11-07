a37fdc1cbd07fe31f0b0a2ac7122450d56a134d4
yns-qara <younesqara@gmail.com>
2022-11-07 22:28:59 +0100

    🙌 structure plan

--- components/About.js
+++ components/About.js
@@ -1,3 +1,17 @@
+// a horizontal roadmap with my achievements and remarcable stops. currently it will include where did I go to school , where did I learn programming , where did I get my first clients , where did I learn new technologies, where did I finish a project...
+
+/*
+ 
+<> I finished the project of  ...
+.
+.
+.
+.
+<> I went to school ...
+
+ */
+
+
 
 const About = () => {
   return (
