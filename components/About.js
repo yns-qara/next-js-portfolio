6de7716170f61ee722b30dde8d30adab525b7d78
yns-qara <younesqara@gmail.com>
2022-11-08 22:54:35 +0100

    👇 About component comming to lide

--- components/About.js
+++ components/About.js
@@ -1,5 +1,8 @@
 // a horizontal roadmap with my achievements and remarcable stops. currently it will include where did I go to school , where did I learn programming , where did I get my first clients , where did I learn new technologies, where did I finish a project...
 
+import Progress from "./Progress"
+import styles from '../styles/about.module.css'
+
 /*
  
 <> I finished the project of  ...
@@ -15,7 +18,19 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.container}>
+      <div>
+        <Progress />
+        <div>
+        </div>
+      </div>
+      <div>
+        <Progress />
+      </div>
+      <div>
+        <Progress />
+      </div>
+    </div>
   )
 }
 
---
+++ components/Progress.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/progress.module.css'
+function Progress() {
+  return (
+    <div className={styles.container}>
+    
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo, esse veritatis enim nisi ducimus impedit natus quae reprehenderit consequuntur, voluptate error aliquid ratione cum cumque deleniti sapiente consectetur. Aut, nulla.
+
+    </div>
+  )
+}
+
+export default Progress
